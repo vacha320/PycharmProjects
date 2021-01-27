d8a29adda8a5ca58c8e3ea61393993573b1eb996
vacha320 <vacha320@gmail.com>
2021-01-27 13:16:00 +0300

    выдает ошибку 403

--- pars.py
+++ pars.py
@@ -3,4 +3,16 @@ from bs4 import BeautifulSoup
 import csv
 
 
-HOST = ''
+HOST = 'https://www.avito.ru/bryansk/'
+URL = 'https://www.avito.ru/bryansk/rabota?cd=1'
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0'
+}
+CSV = ()
+
+def get_html(url,params=''):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+html = get_html(URL)
+print(html.status_code)
